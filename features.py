15936fe7423b1562d17de981560e090aef0c7a5b
Sokra05 <smpaulhz@gmail.com>
2023-11-26 17:03:24 +0100

    comments added

--- features.py
+++ features.py
@@ -1,14 +1,16 @@
 from functions import *
 
 def important_words(dico_tfidf):           #feature 1
+    '''Identifies words that have a TF-IDF score of 0 across all documents in the corpus.'''
     L = []
     del dico_tfidf["files"]
     for k, v in dico_tfidf.items():
-        if sum(v)/len(v) == 0.0:
+        if sum(v)/len(v) > 0:
             L.append(k)
     return L
 
 def most_important_word(dico_tfidf):        #feature 2
+    '''This function finds the word with the highest average TF-IDF score across all documents.'''
     del dico_tfidf["files"]
     maxk = ""
     maxv = 0
@@ -16,27 +18,22 @@ def most_important_word(dico_tfidf):        #feature 2
         if sum(v)/len(v) > maxv:
             maxk = k
             maxv = sum(v)/len(v)
-    return maxk
+    return maxk, maxv
 
-def most_word_of_chirac():        #feature 3
-    fl = ""
-    with open('./cleaned/Nomination_Chirac1.txt', 'r') as f1, open('./cleaned/Nomination_Chirac2.txt', 'r') as f2:
-        ls = f1.readlines()
-        ls2 = f2.readlines()
-        for l in ls:
-            fl += l[:-1] + " "
-        for k in ls2:
-            fl += k[:-1] + " "
-        fl = tf(fl)
-    maxiv = 0
-    maxik = ""
-    for k, v in fl.items():
-        if v >= maxiv:
-            maxiv = v
-            maxik = k
-    return maxik, maxiv
+def most_word_of_chirac(dico_tfidf):        #feature 3
+    '''Reads two files related to Chirac's nominations, combines the text, calculates the term frequency, and returns the word with the highest frequency.'''
+    del dico_tfidf["files"]
+    mink = ""
+    minv = 1000
+    for k, v in dico_tfidf.items():
+        if minv > v[0] + v[1] / 2 > 0:
+            mink = k
+            minv = v[0] + v[1] / 2
+            print(mink, minv)
+    return mink
 
 def nations_word_on_speeches(dico_tfidf):       #feature 4
+    '''This function looks for documents where the word "nation" has a non-zero TF-IDF score and returns a list of titles and the title with the minimum TF-IDF score.'''
     L = []
     minname = ""
     minv = 0
@@ -52,6 +49,7 @@ def nations_word_on_speeches(dico_tfidf):       #feature 4
     return L, names_extract_titles(minname)
 
 def first_pres_to_talk_eco(dico_tfidf):         #feature 5
+    '''Identifies the president who first talked about "climat" or "ecologie" based on the highest TF-IDF score.'''
     minname = ""
     minv = 0
     for k, v in dico_tfidf.items():
@@ -63,7 +61,14 @@ def first_pres_to_talk_eco(dico_tfidf):         #feature 5
                         minv = v[i]
     return minname
 
-def all_pres_say_words():         #feature 6
-    '''Impossible ? If we want the words says by all the presidents we have idf = log(8/8) = log(1) = 0 and the only
-    words with this tf_idf score are the unimportant word...'''
-    return False
+def all_pres_say_words(dico_tfidf):         #feature 6
+    '''Identifies words that are said by all presidents based on non-zero TF-IDF values across different documents.'''
+    L=[]
+    status=True
+    for k, v in dico_tfidf.items():
+        for i in v:
+            if i == 0:
+                status=False
+        if status:
+            L.append(k)
+    return L
